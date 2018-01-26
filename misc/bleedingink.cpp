d25e7a033ffebffba1cf883a421571835869e6e7
Mithun <mithun2595@gmail.com>
2018-01-25 18:22:52 -0800

    indeed bleeding ink

---
+++ misc/bleedingink.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int r,c,num;
+	cin>>r>>c;
+	vector<vector<int> > paper(r, vector<int>(c,0));
+	cin>>num;
+	for(int i=0;i<num;i++) {
+	    int ridx, cidx, darkness;
+	    cin>>ridx>>cidx>>darkness;
+	    for(int p = 0;p<r;p++) {
+	        for(int q = 0;q<c;q++) {
+	            paper[p][q] = max(darkness-abs(ridx - p)-abs(cidx-q), paper[p][q]);
+	        }
+	    }
+	}
+	long sum =0;
+	for(int i=0;i<r;i++)
+	    for(int j=0;j<c;j++) 
+	        sum += paper[i][j];
+	cout<<sum<<"\n";
+	return 0;
+}
+
